808d6c5349cb56a1793856feba1eb7a0cd551fa1
Nikita Baryshev <nbasharkin@gmail.com>
2020-03-11 01:04:59 +0200

    Added comments

--- app.py
+++ app.py
@@ -4,33 +4,38 @@ import csv
 from io import StringIO
 import logic
 
+# initiating the app
 app = flask.Flask(__name__)
 
-
+# creating the route /getAddress
 @app.route('/getAddresses', methods=('GET','POST'))
 def get_addresses():
+    # loading the file and reading it
     csv_file_url = flask.request.form.get("file")
     csv_file_content = requests.get(csv_file_url).text
 
+    # simulating csv file as csv_file_content is a simple text now
     f = StringIO(csv_file_content)
-    reader = csv.reader(f, delimiter=',')
+    reader = csv.reader(f, delimiter=',') # "," set up as a delimiter now
     csv_data = list(reader)
-    csv_data.pop(0)
+    csv_data.pop(0) # excluding heading as we don't need it for the further processing
 
-    points = []
-    links = []
+    points = [] # store all points
+    links = [] # store links
 
+    # iterating through the data in csv
     for row in csv_data:
         name = row[0]
         latitude = float(row[1])
         longitude = float(row[2])
 
-
+        # logic file consist all manipulations with arcgis API and calculating the destination between the links
         points.append({'name': name, 'address': logic.geocoding(latitude, longitude), 'coordinates': (latitude, longitude)})
 
-
+    # combiations of destinations
     processed_distances = []
 
+    # check if destination already exists, if no then we add it
     for point1 in points:
         for point2 in points:
             combinated_name1 = point1['name'] + point2['name']
@@ -44,11 +49,13 @@ def get_addresses():
 
             processed_distances.append(combinated_name1)
 
+            # logic file consist all manipulations with arcgis API and calculating the destination between the links
             links.append({'name': combinated_name1, "distance": logic.destination(tuple(point1['coordinates']), tuple(point2['coordinates']))})
 
-
+    # collecting all results
     result = {'points': points, 'links': links}
 
+    #return the result in JSON format
     return flask.jsonify(result)
 
 
--- logic.py
+++ logic.py
@@ -6,50 +6,23 @@ def geocoding(latitude, longitude):
     """
     Geocodes the address basing on latitude and longitude
     """
+    # accessing the data and the source
     gis = arcgis.gis.GIS("https://www.arcgis.com/", "Niktia", "polo8Ret")
 
+    # reverse_geocode returns the address details
     results = arcgis.geocoding.reverse_geocode([latitude, longitude])
 
     return results
 
 
-#
-# lyon = (45.7597, 4.8422) # (lat, lon)
-# paris = (48.8567, 2.3508)
-#
-# print(haversine(lyon, paris))
-#
-#
 def destination(city1, city2):
+    """
+    :param city1:
+    :param city2:
+    :return: Meters
+    """
 
-    cities_dest = haversine(city1, city2, Unit.METERS)
+    # haversine function calculates the destination basing on longitude and latitude
+    cities_dest = haversine(city1, city2, Unit.METERS) # meters
 
     return cities_dest
-#
-#
-# def convertTuple(tup):
-#     str =  ''.join(tup)
-#     return str
-#
-# list=[]
-# for i in combinations([a['name'],b['name'],c['name']], 2):
-#     cities = convertTuple(i)
-#     # print(cities)
-#     list.append({"name": cities, "destination":haversine((a['latitude'], a['longitude']), (b['latitude'], b['longitude']))})
-# print(list)
-#
-#
-# # a = {'name': "A",
-# #     'latitude': 50.412,
-# #     'longitude': 30.124
-# #     }
-# #
-# # b = {'name': "B",
-# #     'latitude': 50.346,
-# #     'longitude': 30.437,
-# #     }
-# #
-# # c = {'name': "C",
-# #     'latitude': 70.346,
-# #     'longitude': 14.437
-# #     }
